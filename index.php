b0feab1d04b1d2dbf5b0f30587f4c277cb718a0a
daugus <augustodelacamara@gmail.com>
2022-10-12 21:26:19 +0200

    agregado index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang='eu'>
+
+<head>
+  <meta charset='UTF-8' />
+  <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+
+  <title>IGKluba</title>
+
+  <link rel='stylesheet' href='style.css' />
+</head>
+
+<body>
+  <h1>kaixo</h1>
+</body>
+
+</html>
