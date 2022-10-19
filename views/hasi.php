d301e2d1a424da620c2eb467af7f0aadf61ca5f6
“Unai” <unaicabo15@gmail.com>
2022-10-19 09:14:55 +0200

    login terminado

--- views/hasi.php
+++ views/hasi.php
@@ -8,7 +8,7 @@ checkLogin();
 include_once '../templates/head.php';
 include_once '../templates/header.php';
 agregarHead('Saioa hasi | IGKluba', __FILE__);
-headerLogin();
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $apodoEnviado = $_REQUEST['apodo'];
@@ -32,44 +32,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <body>
   <div class="page">
-  <div class="container">
-    <div class="left">
-      <div class="login">Login</div>
-      <div class="eula">By logging in you agree to the ridiculously long terms that you didn't bother to read</div>
-    </div>
-    <div class="right">
-      <svg viewBox="0 0 320 300">
-        <defs>
-          <linearGradient
-                          inkscape:collect="always"
-                          id="linearGradient"
-                          x1="13"
-                          y1="193.49992"
-                          x2="307"
-                          y2="193.49992"
-                          gradientUnits="userSpaceOnUse">
-            <stop
-                  style="stop-color:#ff00ff;"
-                  offset="0"
-                  id="stop876" />
-            <stop
-                  style="stop-color:#ff0000;"
-                  offset="1"
-                  id="stop878" />
-          </linearGradient>
-        </defs>
-        <path d="m 40,120.00016 239.99984,-3.2e-4 c 0,0 24.99263,0.79932 25.00016,35.00016 0.008,34.20084 -25.00016,35 -25.00016,35 h -239.99984 c 0,-0.0205 -25,4.01348 -25,38.5 0,34.48652 25,38.5 25,38.5 h 215 c 0,0 20,-0.99604 20,-25 0,-24.00396 -20,-25 -20,-25 h -190 c 0,0 -20,1.71033 -20,25 0,24.00396 20,25 20,25 h 168.57143" />
-      </svg>
-      <div class="form">
-        <label for="email">Email</label>
-        <input type="email" id="email">
-        <label for="password">Password</label>
-        <input type="password" id="password">
-        <input type="submit" id="submit" value="Submit">
+    <div class="container">
+      <div class="left">
+        <div class="eula">
+          <img src="src/img/soloLogo.png" alt="logo">
+        </div>
+        <div class="login">SAIOA HASI</div>
+        
+          <div class="btn-one">
+            <a  href="/">ITZULI</a>
+          </div>
+        
+      </div>
+      <div class="right">
+        <div class="form" id="formulario">
+          <label for="email" >Ezizena</label>
+          <input type="email" id="email" placeholder="Sartu Email-a">
+          <label for="password" >Pasahitza</label>
+          <input type="password" id="password" placeholder="Sartu pasahitza">
+          <input type="submit" id="submit" value="Bidali">
+        </div>
       </div>
     </div>
   </div>
-</div>
 
   <main class="flex-center">
     <form action="" method="post">
@@ -82,7 +67,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <input type="password" id="pass" name="pass" minlength="1" maxlength="30" required>
       </div>
 
-      <button id="login">Saioa hasi</button>
+      <button id="login">SAIOA HASI</button>
     </form>
   </main>
 </body>
