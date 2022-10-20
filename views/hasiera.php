842a68ae76b267baf083eb524e373caef5499467
“Unai” <unaicabo15@gmail.com>
2022-10-20 09:46:26 +0200

    actualizado estilo de página de inicio y botones de forms

--- views/hasiera.php
+++ views/hasiera.php
@@ -17,6 +17,7 @@ agregarHead('IGKluba', '', false);
   <main id="hasiera">
     <h1>Irakurle Gazteen Kluba</h1>
     <p>Bigarren Hezkuntzako gazte askok irakurtzeko zaletasuna dugu. Hala ere, liburudendetan hainbeste liburu daude, ez dakigula nondik hasi! Webgune honetan gaztetxuentzako eta ez hain gaztetxuentzako liburuak daude: arrakastatsuenak…baita gustatu ez zaizkigunak ere. Bilatu eta gozatu!</p>
+    <img id="collage" src="/src/img/collage-Libros.png" alt="collage">
   </main>
 
   <?php footerInicio() ?>
