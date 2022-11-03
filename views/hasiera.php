b4712b099fd5518fa58267d6dc3f476d83a5f00c
Augusto <augustodelacamara@gmail.com>
2022-11-03 08:15:25 +0100

    centrado botones y texto de página principal

--- views/hasiera.php
+++ views/hasiera.php
@@ -16,21 +16,20 @@ agregarHead('IGKluba', '', false);
 
   <main id="main-hasiera">
 
-    <div class="flex-hasiera-registro">
-
-      <div class="clm-izq-hasiera">
+    <div class="clm-izq-hasiera">
+      <div>
         <h1>Irakurle Gazteen Kluba</h1>
         <p>Bigarren Hezkuntzako gazte askok irakurtzeko zaletasuna dugu. Hala ere, liburudendetan hainbeste liburu daude, ez dakigula nondik hasi! Webgune honetan gaztetxuentzako eta ez hain gaztetxuentzako liburuak daude: arrakastatsuenak…baita gustatu ez zaizkigunak ere. Bilatu eta gozatu!</p>
-       <div class="botones-hasiera">
-        <a href="/sortu" class="btn-hasiera">Sortu kontua</a>
-        <a href="/hasi" class="btn-hasiera">Saioa hasi</a>
-       </div>
+        <div class="botones-hasiera">
+          <a href="/sortu" class="btn-hasiera">Sortu kontua</a>
+          <a href="/hasi" class="btn-hasiera">Saioa hasi</a>
+        </div>
       </div>
-      
+
 
 
     </div>
-    
+
   </main>
 
   <?php agregarFooter() ?>
